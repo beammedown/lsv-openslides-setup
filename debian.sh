fa28dc77df5f684f1b7821635a10ed3e8cf13c41
Beammedown <77543941+Beammedown@users.noreply.github.com>
2023-01-19 22:04:36 +0100

    put docker-install into files themselves

--- debian.sh
+++ debian.sh
@@ -1,55 +1,96 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-ORIGIN_DIR=echo "$PWD"
-echo "Please enter a directory name for OpenSlides to be installed: "
-read DIR_NAME
 
-read -r -p "Do you have a FQDN (eg. example.com)? [Y/N] " response
+echo "#############################################"
+echo "########## OpenSlides 4 Installer ###########"
+echo "#############################################"
+echo "Prüfe auf Updates..."
+echo ###### UPDATING ######
+sudo apt update && sudo apt upgrade -y
+
+echo ###### SELECTING DIRECTORY ######
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Check if Docker is installed
+echo "Prüfe auf Docker..."
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker ist installiert."
+else
+    echo "Installiere Docker bevor du fortfährst!"
+    read -r -p "Soll ich Docker installieren? [Y/N] " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            sudo apt-get update
+
+            sudo apt-get install \
+                ca-certificates \
+                curl \
+                gnupg \
+                lsb-release
+
+            sudo mkdir -p /etc/apt/keyrings
+
+            curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+            echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+            sudo apt-get update
+
+            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+            ;;
+        *)
+            echo "sudo apt install docker.io -y oder schaue auf https://docs.docker.com/engine/install/debian/"
+            exit 1
+            ;;
+    esac
+fi
+
+
+#echo "Please enter a directory name for OpenSlides to be installed: "
+#read DIR_NAME
+echo ###### GATHERING DOMAIN INFORMATION ######
+read -r -p "Hast du eine Domain (bsp. example.com)? [Y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
-        read -p "Please provide this Domain" FQDN
+        read -p "Gib diese Domain bitte an: " FQDN
         ;;
     *)
         FQDN=:80
         ;;
 esac
-
-if [ -d "$DIR_NAME"]; then
-echo "Directory \"$DIR_NAME\" ist already taken up. please remove the directory through 'rm -rf $DIR_NAME' or rename it"
-else
-mkdir $DIR_NAME
-cd $DIR_NAME
-fi
+echo ###### SETTING UP DIRECTORY ######
+#if [ -d DIR_NAME];
+#then
+#    echo "Directory \"$DIR_NAME\" ist already taken up. please remove the directory through 'rm -rf $DIR_NAME' or rename it"
+#else
+#    mkdir $DIR_NAME
+#    cd $DIR_NAME
+#fi
+mkdir os4
+cd os4
+echo ###### INSTALLING NECESSARY DEPENDENCIES ######
 sudo apt install docker.io -y && sudo apt install docker-compose -y && sudo apt install git -y
+echo ###### GETTING OPENSLIDES MANAGE SERVIVE ######
 wget https://github.com/OpenSlides/openslides-manage-service/releases/download/latest/openslides
 chmod +x openslides
 ./openslides setup .
-docker-compose pull
-docker-compose up -d
+docker compose pull
+docker compose up -d
 ./openslides initial-data
+echo ###### OPENSLIDES UP AND RUNNING ######
+echo ###### INSTALLING CADDY ######
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update
 sudo apt install caddy
 cd ..
-echo -en '$FQDN { \n     reverse_proxy https://localhost:8000 { \n               transport http {\n                     tls_insecure_skip_verify\n               }\n     }\n}' > Caddyfile
-if [-f /etc/caddy/Caddyfile]; then
-rm /etc/caddy/Caddyfile
-cp Caddyfile /etc/caddy/Caddyfile
-else
-cp Caddyfile /etc/caddy/Caddyfile
-fi
+echo ###### SETTING UP CADDY ########
+echo -en "$FQDN { \n    reverse_proxy https://localhost:8000 { \n        transport http {\n            tls_insecure_skip_verify\n        }\n    }\n}" > Caddyfile
+if [-f /etc/caddy/Caddyfile]; then rm /etc/caddy/Caddyfile && cp Caddyfile /etc/caddy/Caddyfile; else cp Caddyfile /etc/caddy/Caddyfile; fi
 cd /etc/caddy/
 caddy start
-cd ORIGIN_DIR
-echo "All up and running. Call https://localhost"
-read -p "Do you want to create a crontab that starts OpenSlides on boot? [y/N]" response
-case "$response" in
-    [yY][eE][sS]|[yY])
-        (crontab -l 2>/dev/null; echo "*/5 * * * * $DIR_NAME/startcrontab.sh") | crontab -
-        ;;
-    *)
-        FQDN=:80
-        ;;
-esac
+cd BASEDIR
+echo "All up and running. Call https://$FQDN to access OpenSlides."
+echo "Wenn du OpenSlides herunterfahren möchtest, kannst du einfach in $BASEDIR/os4/ den Befehl 'docker compose down' eingeben."
--- ubuntu.sh
+++ ubuntu.sh
@@ -19,7 +19,25 @@ else
     read -r -p "Soll ich Docker installieren? [Y/N] " response
     case "$response" in
         [yY][eE][sS]|[yY])
-            ${__dir}/install-docker.sh
+            sudo apt-get update
+
+            sudo apt-get install \
+                ca-certificates \
+                curl \
+                gnupg \
+                lsb-release
+
+            sudo mkdir -p /etc/apt/keyrings
+
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+            echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+            sudo apt-get update
+
+            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
             ;;
         *)
             echo "sudo apt install docker.io -y oder schaue auf https://docs.docker.com/engine/install/ubuntu/"
