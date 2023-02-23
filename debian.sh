74470420509b4c956d29f0b5718161794b46c9d5
beammedown <trongero007@gmail.com>
2023-02-23 18:42:46 +0100

    Debian stable

--- debian.sh
+++ debian.sh
@@ -1,8 +1,75 @@
 #!/bin/bash
 
+POSITIONAL_ARGS=()
+
+if [[ $# -eq 0 ]]
+    then
+        echo "lsvops ist ein Tool, welches OpenSlides automatisch installiert und hinter eine Domain verpackt.
+
+Nutzung:
+  ./debian.sh [command]
+
+Verfügbare Commands:
+  -d --directory             Lege das Installationsverzeichnis fest
+  -f --domain                Lege die Domain fest
+  -v --version               Zeigt die Version des Skripts
+
+
+Flags:
+  -h, --help   Hilfe für das Skript
+
+Use 'openslides [command] --help' for more information about a command."
+        exit 0
+fi
+
 echo "#############################################"
 echo "########## OpenSlides 4 Installer ###########"
 echo "#############################################"
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    -d|--directory)
+        DIRECTORY="$2"
+        shift
+        shift
+        ;;
+    -f|--domain)
+        FQDN="$2"
+        shift
+        shift
+        ;;
+    --default)
+        DEFAULT=YES
+        shift
+        ;;
+    -*|--*)
+        echo "Unknown option $1"
+        exit 1
+        ;;
+    *)
+        POSITIONAL_ARGS+=("$1")
+        shift
+        ;;
+    esac
+done
+
+if [[ "$DEFAULT" == "NO" ]]
+then
+    if [[ -z $DIRECTORY ]]
+    then
+            echo "Kein Installationsverzeichnis angegeben. Bitte gib ein Installationsverzeichnis mit der -d oder --directory Option an. Alternativ nutze --default"
+            exit 0
+    elif [[ -z $SEARCHPATH ]]
+    then
+        echo "Keine Domain angegeben. Bitte gib eine Domain mit der -f oder --domain Option an. Alternativ nutze --default"
+        exit 0
+    fi
+else
+    DIRECTORY="os4"
+    FQDN=":80"
+fi
+
+
 echo "Prüfe auf Updates..."
 echo ###### UPDATING ######
 sudo apt update && sudo apt upgrade -y
@@ -16,59 +83,19 @@ if [ -x "$(command -v docker)" ]; then
     echo "Docker ist installiert."
 else
     echo "Installiere Docker bevor du fortfährst!"
-    read -r -p "Soll ich Docker installieren? [Y/N] " response
-    case "$response" in
-        [yY][eE][sS]|[yY])
-            sudo apt-get update
-
-            sudo apt-get install \
-                ca-certificates \
-                curl \
-                gnupg \
-                lsb-release
-
-            sudo mkdir -p /etc/apt/keyrings
-
-            curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-            echo \
-            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-            sudo apt-get update
-
-            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-            ;;
-        *)
-            echo "sudo apt install docker.io -y oder schaue auf https://docs.docker.com/engine/install/debian/"
-            exit 1
-            ;;
-    esac
+    echo "Siehe https://docs.docker.org/install/debian"
 fi
 
-
-#echo "Please enter a directory name for OpenSlides to be installed: "
-#read DIR_NAME
-echo ###### GATHERING DOMAIN INFORMATION ######
-read -r -p "Hast du eine Domain (bsp. example.com)? [Y/N] " response
-case "$response" in
-    [yY][eE][sS]|[yY])
-        read -p "Gib diese Domain bitte an: " FQDN
-        ;;
-    *)
-        FQDN=:80
-        ;;
-esac
 echo ###### SETTING UP DIRECTORY ######
-#if [ -d DIR_NAME];
-#then
-#    echo "Directory \"$DIR_NAME\" ist already taken up. please remove the directory through 'rm -rf $DIR_NAME' or rename it"
-#else
-#    mkdir $DIR_NAME
-#    cd $DIR_NAME
-#fi
-mkdir os4
-cd os4
+if [ -d "$DIRECTORY" ];
+then
+    echo "Directory '${DIRECTORY}' ist already taken up. please remove the directory through 'rm -rf ${DIRECTORY}' or rename it"
+    exit
+else
+    mkdir "$DIRECTORY"
+    cd "$DIRECTORY"
+fi
+
 echo ###### INSTALLING NECESSARY DEPENDENCIES ######
 sudo apt install docker.io -y && sudo apt install docker-compose -y && sudo apt install git -y
 echo ###### GETTING OPENSLIDES MANAGE SERVIVE ######
@@ -76,7 +103,8 @@ wget https://github.com/OpenSlides/openslides-manage-service/releases/download/l
 chmod +x openslides
 ./openslides setup .
 docker compose pull
-docker compose up -d
+docker compose up --detach
+./openslides check-server
 ./openslides initial-data
 echo ###### OPENSLIDES UP AND RUNNING ######
 echo ###### INSTALLING CADDY ######
@@ -91,6 +119,6 @@ echo -en "$FQDN { \n    reverse_proxy https://localhost:8000 { \n        transpo
 if [-f /etc/caddy/Caddyfile]; then rm /etc/caddy/Caddyfile && cp Caddyfile /etc/caddy/Caddyfile; else cp Caddyfile /etc/caddy/Caddyfile; fi
 cd /etc/caddy/
 caddy start
-cd BASEDIR
+cd ${__dir}
 echo "All up and running. Call https://$FQDN to access OpenSlides."
-echo "Wenn du OpenSlides herunterfahren möchtest, kannst du einfach in $BASEDIR/os4/ den Befehl 'docker compose down' eingeben."
+echo "Wenn du OpenSlides herunterfahren möchtest, kannst du einfach in ${__dir}/os4/ den Befehl 'docker compose down' eingeben."
